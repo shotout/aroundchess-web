0de57313fc2f772a6efd4de120d70078a04ff945
ebennazer <eben@kemang.sg>
2025-02-10 14:20:27 +0700

    Update route.ts

--- app/api/chess/monthlyGames/route.ts
+++ app/api/chess/monthlyGames/route.ts
@@ -41,7 +41,7 @@ export async function POST(request: NextRequest) {
     })
 
     return NextResponse.json(newOrUpdatedChessApiMonthlyGame)
-  } catch (dbError) {
+  } catch (dbError:any) {
     // Menampilkan error yang lebih mendetail
     console.error("Database error:", dbError)
     return NextResponse.json({ error: dbError.message || 'Failed to save data to databasesss' }, { status: 500 })
