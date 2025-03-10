ab808711acf95afa423e1f526ca87600141b4544
naufal <naufal@kemang.sg>
2025-03-10 14:49:40 +0700

    fix build without comment anycode

--- components/game-history/DialogButton.tsx
+++ components/game-history/DialogButton.tsx
@@ -80,12 +80,12 @@ const DialogButton: React.FC<DialogButtonProps> = ({ open, setOpen }) => {
   };
 
   const handleButtonClick = () => {
-    // if (activeTab === "upload" && !fileName) {
-    //   fileInputRef?.current.click();
-    // } else {
-    //   // Handle import logic
-    //   setIsSubmitted(true);
-    // }
+    if (activeTab === "upload" && !fileName) {
+      fileInputRef?.current.click();
+    } else {
+      // Handle import logic
+      setIsSubmitted(true);
+    }
     console.log('ada');
   };
 
