1efb6f5f1b1a5a61254b886493eccb232d7ea7a6
naufal <naufal@kemang.sg>
2025-03-12 11:59:34 +0700

    revisi fix

--- app/analysis/AnalysisLatestGame.tsx
+++ app/analysis/AnalysisLatestGame.tsx
@@ -99,7 +99,7 @@ const AnalysisLatestGame: React.FC = () => {
     }
   };
   return (
-    <div className="flex flex-col xl:min-w-[592px] xl:max-w-full gap-4 bg-white mt-2 lg:mt-0 lg:border lg:border-input lg:rounded-lg mb-2 sm:mb-4">
+    <div className="flex flex-col xl:min-w-[592px] xl:max-w-[full] gap-4 bg-white mt-2 lg:mt-0 lg:border lg:border-input lg:rounded-lg mb-2 sm:mb-4">
       <div className="flex flex-col px-4 gap-2">
         <span className="text-sm sm:text-md md:text-lg lg:text-xl pt-4 font-bold">
           Analysis: Latest Game
