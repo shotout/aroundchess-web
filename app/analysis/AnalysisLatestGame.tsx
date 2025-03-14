0a9fc16592e654430ebc7d9e1a7ec690eb87b7d3
naufal <naufal@kemang.sg>
2025-03-14 11:05:02 +0700

    perbaikan padding

--- app/analysis/AnalysisLatestGame.tsx
+++ app/analysis/AnalysisLatestGame.tsx
@@ -100,7 +100,7 @@ const AnalysisLatestGame: React.FC = () => {
   };
   return (
     <div className="flex flex-col xl:min-w-[592px] xl:max-w-full gap-4 bg-white mt-0 lg:mt-0 lg:border lg:border-input lg:rounded-lg mb-2 sm:mb-4">
-      <div className="flex flex-col px-4 gap-2">
+      <div className="flex flex-col px-4 gap-2 py-2">
         <span className="text-sm sm:text-md md:text-lg lg:text-xl font-bold">
           Analysis: Latest Game
         </span>
