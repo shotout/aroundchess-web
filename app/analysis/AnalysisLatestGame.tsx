8846ebf8f47a0f493b6f8037a87102ac78b02b6e
naufal <naufal@kemang.sg>
2025-03-17 14:05:15 +0700

    perbaikan revisi

--- app/analysis/AnalysisLatestGame.tsx
+++ app/analysis/AnalysisLatestGame.tsx
@@ -102,10 +102,10 @@ const AnalysisLatestGame: React.FC = () => {
   return (
     <div className={` ${hideDiv && "mt-96 sm:mt-[64%]"} flex flex-col xl:min-w-[592px] xl:max-w-full gap-4 bg-white mt-0 lg:mt-0 lg:border lg:border-input lg:rounded-lg mb-2 sm:mb-4`}>
       <div className="flex flex-col px-4 gap-2 py-2">
-        <span className="text-sm sm:text-md md:text-lg lg:text-xl font-bold">
+        <span className="text-sm sm:text-md md:text-lg lg:text-lg font-bold">
           Analysis: Latest Game
         </span>
-        <span className="text-xs sm:text-sm md:text-md lg:text-lg">
+        <span className="text-xs sm:text-sm md:text-md lg:text-md">
           {gameInfo?.date}, {summary?.whiteSide?.profileInfo.username} (White
           <span className="text-[#00B427]">
             {gameInfo?.whiteWin && " - WIN"}
