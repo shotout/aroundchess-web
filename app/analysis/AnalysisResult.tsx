a7ae3ef56db344ed2fdb8fdcf61ca7303fef74fc
Yoga Ardli Ardetya <yoga@kemang.sg>
2025-03-18 09:10:37 +0700

    fix import button, integrate with mock json

--- app/analysis/AnalysisResult.tsx
+++ app/analysis/AnalysisResult.tsx
@@ -348,10 +348,7 @@ const AnalysisResult: React.FC = () => {
     const height = window.innerHeight;
     const isPortrait = height > width;
     const minPadding = 0;
-    const maxSize =
-      window.innerWidth > 1300
-        ? window.innerWidth / 4.5
-        : 453;
+    const maxSize = window.innerWidth > 1300 ? window.innerWidth / 4.5 : 453;
     // const maxSize = window.innerWidth > 1300 ? 453 : window.innerWidth/1.5;
     console.log("Resizing board...", isPortrait);
 
