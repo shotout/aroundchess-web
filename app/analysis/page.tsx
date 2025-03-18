45f3cc17dd0924df58810c64858af37aec676b8b
naufal <naufal@kemang.sg>
2025-03-18 13:22:49 +0700

    perbaikan import games analysis page

--- app/analysis/page.tsx
+++ app/analysis/page.tsx
@@ -6,8 +6,9 @@ import { useEffect, useState } from "react";
 import { usePgnStore } from "../store/zustandStore";
 import { motion } from "framer-motion";
 import { AnalyzeDifferentGame } from "@/components/modal/AnalyzeDifferentGame";
+import LoadingPage from "@/components/analysis-loading/LoadingPage";
 export default function AnalysisPage() {
-  const { setHideDiv, hideDiv } = usePgnStore(); // Get PGN from the Zustand store
+  const { setHideDiv, hideDiv, isLoading } = usePgnStore(); // Get PGN from the Zustand store
 
   const [isVisible, setIsVisible] = useState<boolean>(true);
   let lastScrollY = 0;
@@ -30,35 +31,47 @@ export default function AnalysisPage() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [lastScrollY]);
   return (
-    <Navigation>
-      <div className="flex flex-col overflow-y-auto">
-        <div
-          className={`flex flex-col bg-white px-2 sm:px-4 md:px-6 lg:px-6 pb-2 sm:pb-4 md:pb-6 lg:pb-8 ${
-            hideDiv && "hidden"
-          }`}
-        >
-          <h2 className="text-md pt-4 text-center lg:text-left sm:text-lg md:text-xl lg:text-2xl font-bold">
-            Analysis Result from{" "}
-            <span className="text-[#4E7838]">Chess.com</span>
-          </h2>
-          <span className="hidden lg:block text-xs sm:text-sm md:text-md lg:text-lg">
-            Discover an Analysis of your latest Chess.com Game.
-          </span>
-          <div className="hidden lg:block text-xs sm:text-sm md:text-md lg:text-lg">
-            AI-powered chess analysis provides deep insights into positional and
-            tactical aspects of a game. It evaluates piece coordination, pawn
-            structure, king safety, and overall positional advantages, helping
-            players understand strategic strengths and weaknesses
+    <>
+      {isLoading == true ? (
+        <LoadingPage />
+      ) : (
+        <Navigation>
+          <div className="flex flex-col overflow-y-auto">
+            <div
+              className={`flex flex-col bg-white px-2 sm:px-4 md:px-6 lg:px-6 pb-2 sm:pb-4 md:pb-6 lg:pb-8 ${
+                hideDiv && "hidden"
+              }`}
+            >
+              <h2 className="text-md pt-4 text-center lg:text-left sm:text-lg md:text-xl lg:text-2xl font-bold">
+                Analysis Result from{" "}
+                <span className="text-[#4E7838]">Chess.com</span>
+              </h2>
+              <div className="xl:hidden flex items-center justify-center mt-2">
+                <AnalyzeDifferentGame />
+              </div>
+              <span className="hidden lg:block text-xs sm:text-sm md:text-md lg:text-lg">
+                Discover an Analysis of your latest Chess.com Game.
+              </span>
+              <div className="hidden xl:flex flex-row items-center justify-between">
+                <div className="hidden lg:block w-3/4 text-xs sm:text-sm md:text-md lg:text-lg">
+                  AI-powered chess analysis provides deep insights into
+                  positional and tactical aspects of a game. It evaluates piece
+                  coordination, pawn structure, king safety, and overall
+                  positional advantages, helping players understand strategic
+                  strengths and weaknesses
+                </div>
+                <AnalyzeDifferentGame />
+              </div>
+            </div>
+            <div className="flex flex-col xl:flex-row-reverse gap-4 bg-white px-4">
+              <AnalysisResult />
+              <div className="xl:w-3/4">
+                <AnalysisLatestGame />
+              </div>
+            </div>
           </div>
-          <AnalyzeDifferentGame />
-        </div>
-        <div className="flex flex-col xl:flex-row-reverse gap-4 bg-white px-4">
-          <AnalysisResult />
-          <div className="xl:w-3/4">
-            <AnalysisLatestGame />
-          </div>
-        </div>
-      </div>
-    </Navigation>
+        </Navigation>
+      )}
+    </>
   );
 }
