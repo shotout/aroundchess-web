66c45a7fb410d10f1f7a37a35ebda4f596aec651
naufal <naufal@kemang.sg>
2025-04-09 14:58:57 +0700

    perbaikan UI

--- components/mistake-log/SavedMistakes.tsx
+++ components/mistake-log/SavedMistakes.tsx
@@ -69,9 +69,9 @@ const SavedMistakes: React.FC = () => {
       case "Blunder":
         return "border border-[#FA402D] text-[#FA402D]";
       case "Mistake":
-        return "border border-[#FFA459] text-[#FFA459]";
+        return "border border-[#FFA459] text-[#FA402D]";
       default:
-        return "border border-[#80B64D] text-[#80B64D]";
+        return "border border-[#80B64D] text-[#FA402D]";
     }
   };
   const getScoreClass = (type: string) => {
@@ -83,17 +83,17 @@ const SavedMistakes: React.FC = () => {
       case "Best":
         return "text-[#364152]";
       case "Miss":
-        return "text-[#FD0000]";
+        return "text-[#E22B32]";
       case "Blunder":
-        return "text-[#FD0000]";
+        return "text-[#E22B32]";
       case "Mistake":
-        return "text-[#FD0000]";
+        return "text-[#E22B32]";
       default:
-        return "text-[#364152]";
+        return "text-[#E22B32]";
     }
   };
   return (
-    <div className="flex flex-col w-full justify-center gap-4 bg-white lg:justify-start xl:max-h-[800px] xl:min-h-[800px] lg:overflow-auto">
+    <div className="flex flex-col w-full justify-center gap-4 rounded-[8px] bg-white lg:justify-start xl:max-h-[800px] xl:min-h-[800px] lg:overflow-auto">
       {savedMistakes.map((item: any, index: number) => {
         return (
           <div
@@ -108,20 +108,20 @@ const SavedMistakes: React.FC = () => {
                   : `border-input`
               } rounded-md p-2 lg:p-4`}
             >
-              <div className="flex flex-row justify-between gap-2 mb-4">
-                <div className="flex rounded-full bg-[#25CEDA] lg:py-1 px-3 justify-center items-center font-semibold text-xs lg:text-sm">
+              <div className="flex flex-row justify-between items-center gap-2 mb-4">
+                <div className="flex rounded-full max-h-[28px] bg-[#25CEDA] lg:py-1 px-3 justify-center items-center font-medium text-xs lg:text-[14px]">
                   VS{" "}
                   {item?.game_result.opponent +
                     " " +
                     item?.game_result.date +
                     " - "}
-                  <span className="text-primary">
+                  <span className="text-[#221AE9] font-semibold">
                     {item?.game_result.status}
                   </span>
                 </div>
-                <div className="rounded-lg bg-[#E6F7FE] border border-[#C6EEFE] py-2 px-3 items-center font-semibold">
+                <div className="rounded-lg bg-[#E6F7FE] border border-[#C6EEFE] p-[10px] items-center font-semibold">
                   <BookmarkFilledIcon
-                    className="w-4 h-4 lg:w-6 lg:h-6"
+                    className="w-[12px] h-[12px] lg:w-[20px] lg:h-[20px]"
                     color="#221AE9"
                   />
                 </div>
@@ -134,44 +134,44 @@ const SavedMistakes: React.FC = () => {
                       src={"/icons/alert-triangle.png"}
                       width={1000}
                       height={1000}
-                      className="w-3 h-3 sm:w-5 sm:h-5 lg:w-6 lg:h-6"
+                      className="w-[12px] h-[12px] sm:w-[14px] sm:h-[14px] lg:w-[18px] lg:h-[18px]"
                     />
                     <div className="flex flex-col">
-                      <span className="text-xs">Mistake Type:</span>
-                      <span className="text-sm font-bold">
+                      <span className="text-[12px]">Mistake Type:</span>
+                      <span className="text-[12px] font-semibold">
                         {item?.mistake.type}
                       </span>
                     </div>
                   </div>
                   <div className="flex flex-row items-center gap-1">
                     <InfoIcon
-                      className="w-3 h-3 sm:w-5 sm:h-5 lg:w-6 lg:h-6"
+                      className="w-[12px] h-[12px] sm:w-[14px] sm:h-[14px] lg:w-[18px] lg:h-[18px]"
                       color="#221AE9"
                     />
                     <div className="flex flex-col">
-                      <span className="text-xs">Game Phase:</span>
-                      <span className="text-sm font-bold">
+                      <span className="text-[12px]">Game Phase:</span>
+                      <span className="text-[12px] font-semibold">
                         {item?.mistake.game_phase}
                       </span>
                     </div>
                   </div>
                 </div>
                 <div className="flex flex-row items-center lg:justify-start gap-3">
-                  <span className="flex items-center text-[12px] sm:text-sm md:text-md lg:text-md font-normal border border-primary rounded-[4px] p-1">
+                  <span className="flex items-center text-[12px] font-normal max-h-[25px] sm:text-sm md:text-md lg:text-md font-normal border border-[#221AE9] rounded-[4px] py-[4px] px-[8px]">
                     Move {item?.mistake.move}:{" "}
-                    <span className="font-bold sm:text-sm md:text-md lg:text-md ">
+                    <span className="font-normal sm:text-sm md:text-md lg:text-md ">
                       {item?.mistake.move}
                     </span>
                   </span>
                   <span
-                    className={`flex items-center rounded-full border border-input px-4 py-1 font-semibold text-xs sm:text-sm md:text-md lg:text-md  text-center font-normal ${getScoreClass(
+                    className={`flex items-center rounded-full border border-[#DEDEDE] px-[8px] py-[4px] font-semibold text-xs sm:text-sm md:text-md lg:text-md text-center font-normal ${getScoreClass(
                       item?.mistake.classification
                     )}`}
                   >
                     {item?.mistake.evaluation}
                   </span>
                   <span
-                    className={`flex items-center min-w-[72px] text-center px-2 py-1 rounded-[4px] text-sm sm:text-sm md:text-md lg:text-md  ${getBadgeClass(
+                    className={`flex items-center justify-center min-w-[72px] text-center px-[8px] py-[4px] rounded-[4px] text-sm sm:text-sm md:text-md lg:text-md  ${getBadgeClass(
                       item?.mistake.classification
                     )}`}
                   >
@@ -179,8 +179,8 @@ const SavedMistakes: React.FC = () => {
                   </span>
                 </div>
               </div>
-              <span className="text-xs sm:text-md md:text-md lg:text-lg  font-normal">
-                <span className="font-bold">Analysis: </span>
+              <span className="text-xs sm:text-md md:text-md lg:text-[14px] font-normal">
+                <span className="font-semibold">Analysis: </span>
                 {item?.analysis}
               </span>
               <div className="p-3 rounded-lg border border-blue-300 bg-gradient-to-r from-blue-50 to-white flex items-center space-x-2">
@@ -192,7 +192,7 @@ const SavedMistakes: React.FC = () => {
                     height={1000}
                     className="w-6 h-6 sm:w-4 sm:h-4 md:w-6 md:h-6 lg:w-8 lg:h-8"
                   />
-                  <span className="font-normal text-xs sm:text-sm md:text-md lg:text-md xl:text-md font-normal text-primary">
+                  <span className="font-normal text-xs sm:text-sm md:text-md lg:text-md xl:text-md font-normal text-[#221AE9]">
                     Recommended Training Exercise:{" "}
                     <span className="font-bold">
                       {" " + item?.recommended_training.title}
@@ -205,23 +205,23 @@ const SavedMistakes: React.FC = () => {
                   return (
                     <div
                       key={index}
-                      className="rounded-sm flex flex-col justify-between border border-input p-2"
+                      className="rounded-[4px] flex flex-col justify-between border border-input p-[12px]"
                     >
-                      <span className="block my-1 font-bold text-xs sm:text-sm">
+                      <span className="block my-1 font-semibold text-xs sm:text-[12px] text-black">
                         {resource.title}
                       </span>
-                      <span className="block my-1 text-[#585858] font-light text-xs sm:text-sm">
+                      <span className="block my-1 text-[#364152] font-light text-xs sm:text-[11px]">
                         {resource.description}
                       </span>
                       <Link href={resource.link}>
                         <div
-                          className="flex flex-row w-full justify-center my-2 sm:my-1 items-center px-4 py-2 rounded-full border border-[#C6EEFE] bg-[#E6F7FE]"
+                          className="btn-tertiary rounded-full flex items-center justify-center"
                           style={{
                             boxShadow: `inset 0px -2px 2px #C6EEFE,
                                          inset 0px 2px 0px #FFFFFF`, // Custom inner shadow
                           }}
                         >
-                          <span className="text-center text-xs sm:text-sm text-primary font-medium">
+                          <span className="text-center text-xs sm:text-[14px] text-[#221AE9] font-medium">
                             Visit {resource.link.replace("https://www.", "")}
                           </span>
                         </div>
