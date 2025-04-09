995959b6310c5f771d1fef4d70a558ffe86b3e98
naufal <naufal@kemang.sg>
2025-04-09 13:35:56 +0700

    implementasi api play-vs-ai

--- functions/format-date.tsx
+++ functions/format-date.tsx
@@ -9,3 +9,14 @@ export const formatDate = (date: string) => {
 
   return dayName + ", " + [day, month, year].join("/");
 };
+export const formatDateHistory = (date: string) => {
+  let d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+  let dayName = d.toLocaleDateString("EN", { weekday: "long" });
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [day, month, year].join("/");
+};
