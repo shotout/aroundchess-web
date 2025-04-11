81d696940fcafa936db092d1998823718ff5e435
naufal <naufal@kemang.sg>
2025-04-11 18:58:43 +0700

    + 3d board

--- components/chessboard/3d/ThreeDChessboard.tsx
+++ components/chessboard/3d/ThreeDChessboard.tsx
@@ -1,27 +1,58 @@
-import { useChessBoardThemeStore } from "@/app/store/chessBoardTheme";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Chessboard } from "react-chessboard";
+import { useMemo } from "react";
 import { Square } from "chess.js";
 import Image from "next/image";
-import React, { useMemo } from "react";
+import {
+  BoardOrientation,
+  PromotionPieceOption,
+} from "react-chessboard/dist/chessboard/types";
+import { useChessBoardThemeStore } from "@/app/store/chessBoardTheme";
 
-import { Chessboard } from "react-chessboard";
-import { BoardOrientation } from "react-chessboard/dist/chessboard/types";
-
-interface ThreeDChessboardProps {
+interface ThreeDBoardProps {
   position: string;
   boardWidth: number;
   orientation: BoardOrientation | undefined;
-  onPieceDrop: (
-    sourceSquare: Square,
-    targetSquare: Square,
-    piece: string
+  // onPieceDrop?: (
+  //   sourceSquare: Square,
+  //   targetSquare: Square,
+  //   piece: string
+  // ) => boolean;
+  arePiecesDraggable?: boolean;
+  onSquareClick: (square: Square) => void;
+  onSquareRightClick: (square: Square) => void;
+  onPromotionPieceSelect: (
+    piece?: PromotionPieceOption,
+    promoteFromSquare?: Square,
+    promoteToSquare?: Square
   ) => boolean;
+
+  promotionToSquare: Square | null;
+  showPromotionDialog: boolean;
+  customSquareStyles?: Record<string, React.CSSProperties>;
+  customArrows: any;
+  areArrowsAllowed: boolean;
+  customArrowColor: string;
 }
 
-const ThreeDChessboard: React.FC<ThreeDChessboardProps> = ({
+const ThreeDBoard: React.FC<ThreeDBoardProps> = ({
   position,
   boardWidth,
   orientation,
-  onPieceDrop,
+  // onPieceDrop,
+  arePiecesDraggable = true,
+  onSquareClick,
+  onSquareRightClick,
+  onPromotionPieceSelect,
+  promotionToSquare,
+  showPromotionDialog,
+  customSquareStyles,
+  customArrows,
+  areArrowsAllowed,
+  customArrowColor,
 }) => {
   const {
     StyleChoosed,
@@ -31,55 +62,98 @@ const ThreeDChessboard: React.FC<ThreeDChessboardProps> = ({
     PieceChoosed,
     setPieceChoosed,
   } = useChessBoardThemeStore();
-  const twoDPieces = useMemo(() => {
+  // Board boardWidth configuration
+  const [boardSize, setBoardSize] = useState<number | any>(700); // Default boardWidth
+  const [scale, setScale] = useState<number | any>(0); // Default boardWidth
+
+  useEffect(() => {
+    console.log(boardWidth, "boardWidth in 3d board wood");
+    console.log(window?.innerWidth, "widthC in 3d board wood");
+    if (boardWidth) {
+      let fixScale = boardWidth / 480;
+
+      setScale(fixScale);
+      let sz = boardWidth < 480 ? 480 : boardWidth;
+      setBoardSize(boardWidth);
+    } else {
+      handleResize();
+    }
+  }, [boardWidth, window?.innerWidth]);
+  const handleResize = () => {
+    const width = window?.innerWidth;
+    const height = window?.innerHeight;
+    const isPortrait = height > width;
+    const minPadding = 0;
+    const maxSize = window?.innerWidth / 3;
+    console.log("Resizing board...", window?.innerWidth, isPortrait);
+
+    if (isPortrait) {
+      // In portrait mode, use screen width as the primary constraint
+      const availableWidth = width - minPadding * 2;
+      // Use 85% of available width for mobile, 90% for tablets
+      const sizeFactor = width <= 430 ? 0.85 : 0.9;
+      setBoardSize(Math.min(maxSize, availableWidth * sizeFactor + 20));
+      console.log(Math.min(maxSize, availableWidth * sizeFactor));
+    } else {
+      // In landscape, use height as the primary constraint
+      const availableHeight = height - minPadding * 2;
+      // Use 80% of available height
+      setBoardSize(Math.min(maxSize, availableHeight * 0.8));
+      console.log(
+        "boardWidth board...",
+        Math.min(maxSize, availableHeight * 0.8)
+      );
+    }
+  };
+  const threeDPieces = useMemo(() => {
     const pieces = [
       {
         piece: "wP",
-        pieceHeight: 1.6,
+        pieceHeight: 1.4,
       },
       {
         piece: "wN",
-        pieceHeight: 1.6,
+        pieceHeight: 1.4,
       },
       {
         piece: "wB",
-        pieceHeight: 1.6,
+        pieceHeight: 1.4,
       },
       {
         piece: "wR",
-        pieceHeight: 1.6,
+        pieceHeight: 1.4,
       },
       {
         piece: "wQ",
-        pieceHeight: 1.6,
+        pieceHeight: 1.5,
       },
       {
         piece: "wK",
-        pieceHeight: 1.6,
+        pieceHeight: 1.4,
       },
       {
         piece: "bP",
-        pieceHeight: 1.6,
+        pieceHeight: 1.4,
       },
       {
         piece: "bN",
-        pieceHeight: 1.6,
+        pieceHeight: 1.4,
       },
       {
         piece: "bB",
-        pieceHeight: 1.6,
+        pieceHeight: 1.4,
       },
       {
         piece: "bR",
-        pieceHeight: 1.6,
+        pieceHeight: 1.4,
       },
       {
         piece: "bQ",
-        pieceHeight: 1.6,
+        pieceHeight: 1.5,
       },
       {
         piece: "bK",
-        pieceHeight: 1.6,
+        pieceHeight: 1.4,
       },
     ];
     const pieceComponents: {
@@ -88,78 +162,135 @@ const ThreeDChessboard: React.FC<ThreeDChessboardProps> = ({
         square,
       }: {
         squareWidth: number;
-        square: string;
+        square: Square;
       }) => JSX.Element;
     } = {};
-    pieces.forEach(({ piece, pieceHeight }) => {
+    pieces.forEach(({ piece, pieceHeight }, index) => {
       pieceComponents[piece] = ({ squareWidth, square }) => (
         <div
+          key={index}
           style={{
-            width: squareWidth,
+            width: squareWidth * pieceHeight,
             height: squareWidth,
             position: "relative",
             pointerEvents: "none",
           }}
         >
           <img
-            src={`/3d-pieces/wood/${piece}.png`}
-            // src={`/pieces/${PieceChoosed}/${piece}.png`}
-            width={squareWidth}
+            src={`/3d-pieces/${PieceChoosed}/${piece}.png`}
+            width={squareWidth * pieceHeight}
             height={squareWidth}
             style={{
               position: "absolute",
-              bottom: `${0.2 * squareWidth}px`,
-              objectFit: "contain",
+              bottom: `${0.12 * squareWidth}px`,
             }}
           />
         </div>
       );
     });
     return pieceComponents;
-  }, [PieceChoosed]);
-  return (
-    <div className="relative" style={{ width: boardWidth, height: boardWidth }}>
-      {/* <span className="text-white bg-[red]">{boardWidth / 8}</span> */}
-      <Image
-        src={`/boards/3d-${BoardChoosed}-board.png`}
-        alt="wood"
-        width={1000}
-        height={1000}
-        style={{ top: Math.round(boardWidth / 36) }}
-        className={`absolute z-2 w-[${boardWidth}] h-[${boardWidth}] object-contain`}
-      />
+  }, []);
+  // Frame dimensions
+  let paddingTop = 50;
 
+  return (
+    <div
+      className="flex flex-col items-center justify-center"
+      style={{
+        width: boardWidth,
+        height: boardWidth,
+        transform: `scale(${scale + ``})`,
+      }}
+    >
       <div
+        className="relative flex items-center justify-center"
         style={{
-          width: boardWidth,
-          height: boardWidth,
-          paddingLeft: Math.round(boardWidth / 7.7),
+          width: 480,
+          height: 480,
         }}
-        className={`z-10 flex`}
       >
-        <Chessboard
-          onPieceDrop={onPieceDrop}
-          boardOrientation={orientation}
-          boardWidth={Math.round(boardWidth - boardWidth / 4.1)}
-          arePiecesDraggable={false}
-          position={position}
-          customBoardStyle={{
-            transform: "rotateX(27.5deg)",
-            transformOrigin: "center",
-          }}
-          customPieces={twoDPieces}
-          customLightSquareStyle={{
-            backgroundColor: "#ff000070",
-            // backgroundColor: "transparent",
-          }}
-          customDarkSquareStyle={{
-            backgroundColor: "#00ff0080",
-            // backgroundColor: "transparent",
-          }}
-          animationDuration={100}
+        <Image
+          src={`/boards/3d-${BoardChoosed}-board.png`}
+          alt="Chess board frame"
+          width={1000}
+          height={1000}
+          className={`absolute z-2 w-[480px] h-[480px] object-contain`}
         />
+        <div
+          style={{
+            marginTop: -paddingTop,
+          }}
+          className={`z-10 flex items-center justify-center`}
+        >
+          {arePiecesDraggable != null ? (
+            <Chessboard
+              arePiecesDraggable={arePiecesDraggable}
+              customArrowColor={customArrowColor}
+              boardOrientation={orientation}
+              boardWidth={Math.round(480 * 0.779)}
+              id="Styled3DBoard"
+              position={position}
+              onSquareClick={onSquareClick}
+              onSquareRightClick={onSquareRightClick}
+              onPromotionPieceSelect={(
+                piece,
+                promoteFromSquare,
+                promoteToSquare
+              ) => {
+                if (piece) {
+                  return onPromotionPieceSelect(piece);
+                }
+                return false;
+              }}
+              customBoardStyle={{
+                transform: "rotateX(27.5deg) scale(1)",
+                transformOrigin: "center",
+                // background:"black"
+              }}
+              customPieces={threeDPieces}
+              customLightSquareStyle={{
+                backgroundColor: "transparent",
+                // backgroundColor: "#00000080",
+              }}
+              customDarkSquareStyle={{
+                backgroundColor: "transparent",
+                // backgroundColor: "#00000070",
+              }}
+              customArrows={customArrows}
+              areArrowsAllowed={areArrowsAllowed}
+              customSquareStyles={customSquareStyles}
+              promotionToSquare={promotionToSquare}
+              showPromotionDialog={showPromotionDialog}
+              animationDuration={200}
+            />
+          ) : (
+            <Chessboard
+              arePiecesDraggable={false}
+              boardOrientation={orientation}
+              boardWidth={Math.round(480 * 0.779)}
+              id="Styled3DBoard"
+              // position={position}
+              customBoardStyle={{
+                transform: "rotateX(27.5deg) scale(1)",
+                transformOrigin: "center",
+                // background:"black"
+              }}
+              customPieces={threeDPieces}
+              customLightSquareStyle={{
+                backgroundColor: "transparent",
+                // backgroundColor: "#00000080",
+              }}
+              customDarkSquareStyle={{
+                backgroundColor: "transparent",
+                // backgroundColor: "#00000070",
+              }}
+              animationDuration={100}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
 };
-export default ThreeDChessboard;
+
+export default ThreeDBoard;
