309d5a78b8866f5ba01a9c07acc3e8ee124a7120
ebennazer <eben@kemang.sg>
2025-04-12 11:45:26 +0700

    Update page.tsx

--- app/analysis/page.tsx
+++ app/analysis/page.tsx
@@ -11,6 +11,7 @@ import { useApiClient } from "@/functions/api-client";
 import DotSpinner from "@/components/game-history/Spinner";
 import ChessAccountSetup from "@/components/analysis/onboarding/ChessAccountSetup";
 
+
 const DEV_MODE = false;
 
 export default function AnalysisPage() {
