fe6dc2b5394e89b898d869231312f8284c9cca82
naufal <naufal@kemang.sg>
2025-04-22 13:23:55 +0700

    perbaikan offside

--- components/navigator/header.tsx
+++ components/navigator/header.tsx
@@ -46,7 +46,7 @@ const Header: React.FC<HeaderProps> = ({ onSidebarToggle }) => {
     setTabType(type);
   };
   return (
-    <header className="fixed xl:sticky top-0 z-30 flex w-full items-center justify-between bg-white px-6 border-b h-[72px]  lg:h-[97px]">
+    <header className="fixed xl:sticky top-0 z-100 flex w-full items-center justify-between bg-white px-6 border-b h-[72px]  lg:h-[97px]">
       {/* Left section - Logo and navigation (on desktop only) */}
       <div className="flex items-center h-[70px] lg:h-[100px]">
         {/* Logo/Title - Always visible */}
