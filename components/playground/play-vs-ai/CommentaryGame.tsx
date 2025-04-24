a78bcfe65c9b6c1fa56e7e90c3b05bc4d8c40f26
naufal <naufal@kemang.sg>
2025-04-24 13:43:21 +0700

    merapikan play vs ai

---
+++ components/playground/play-vs-ai/CommentaryGame.tsx
@@ -0,0 +1,67 @@
+import { fadeInUp, motion } from "@/utils/motion";
+
+import Image from "next/image";
+interface CommentarGameProps {
+  statusGame: string;
+}
+export const CommentarGame = ({ statusGame }: CommentarGameProps) => {
+  let gradColor =
+    statusGame == "Win"
+      ? `bg-[linear-gradient(to_right,_#FFFFFF58,_#00B427,_#00B427,_#00B427,_#00B427,_#00B427,_#00B427,_#FFFFFF40)]`
+      : statusGame == "Draw"
+      ? `bg-[linear-gradient(to_right,_#FFFFFF58,_#221AE9,_#221AE9,_#221AE9,_#221AE9,_#221AE9,_#221AE9,_#FFFFFF40)]`
+      : `bg-[linear-gradient(to_right,_#FFFFFF58,_#C01B1B,_#C01B1B,_#C01B1B,_#C01B1B,_#C01B1B,_#C01B1B,_#FFFFFF40)]`;
+  let color =
+    statusGame == "Win"
+      ? "#00B427"
+      : statusGame == "Draw"
+      ? "#221AE9"
+      : "#C01B1B";
+  let icon =
+    statusGame == "Win"
+      ? "you-win"
+      : statusGame == "Draw"
+      ? "you-draw"
+      : "you-loss";
+  let sparks =
+    statusGame == "Win"
+      ? "sparks-win"
+      : statusGame == "Draw"
+      ? "sparks-draw"
+      : "sparks-loss";
+
+  let content =
+    statusGame == "Win"
+      ? "Congratulations! You won this game!"
+      : statusGame == "Draw"
+      ? "The Game ended in a Draw."
+      : "The Game ended You loss";
+  return (
+    <motion.div
+      variants={fadeInUp}
+      className={`relative justify-self-center w-[95%] mt-4 rounded-[8px] ${gradColor} border border-[${color}] p-[1px]`}
+    >
+      <div
+        className={`flex h-[56px] flex-row items-center rounded-[8px] border-2 border-dashed border-[${color}] gap-3`}
+      >
+        <Image
+          src={`/images/play-vs-ai/${icon}.png`}
+          alt="icon"
+          width={1000}
+          height={1000}
+          className="w-[30px] h-[30px] object-contain m-4 mr-0"
+        />
+        <span className="font-medium text-[14px] text-white">{content}</span>
+        <div className="absolute right-0 top-0 bottom-1 h-full flex items-center justify-center">
+          <Image
+            src={`/images/play-vs-ai/${sparks}.png`}
+            alt="icon"
+            width={1000}
+            height={1000}
+            className="w-full h-[56px] object-cover"
+          />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
