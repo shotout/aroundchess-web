5c1fbb92f35cc66e42294bf0a7fd64adca7541df
naufal <naufal@kemang.sg>
2025-04-25 10:37:25 +0700

    perbaikan jarak simple chess

--- components/handbooks/components/Simple2DChess.tsx
+++ components/handbooks/components/Simple2DChess.tsx
@@ -113,7 +113,7 @@ const Simple2DChess: React.FC<Simple2DChessProps> = ({
             height={squareWidth}
             style={{
               position: "absolute",
-              bottom: 0,
+              bottom: -1.5,
               objectFit: "contain",
               width: "100%",
               height: "auto",
