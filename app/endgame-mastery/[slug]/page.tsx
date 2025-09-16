ecbd5c3fcdbd6df879bb75ed97213a479ee878c9
ebennazer <eben@kemang.sg>
2025-09-16 11:32:53 +0700

    Update page.tsx

--- app/endgame-mastery/[slug]/page.tsx
+++ app/endgame-mastery/[slug]/page.tsx
@@ -4,7 +4,13 @@ import Navigation from "@/components/navigator/navigation";
 import { useMemo } from "react";
 import { useEndgameStore } from "../EndgameStore";
 
-export default function Page({ params }: { params: { slug: string } }) {
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function Page({ params }:PageProps) {
   const endgameStore = useMemo(() => useEndgameStore, []);
 
   return (
