97cec2aa61b3715632d072ba859a938cfe152b32
Gerry <gerry.ahmadi@gmail.com>
2025-09-22 22:27:27 +0700

    prepare deployment

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -3,6 +3,7 @@
 import About from "@/components/about-us/About";
 import Navigation from "@/components/navigator/navigation";
 export default function AboutUs() {
+  
   return (
     <Navigation>
       <About />
