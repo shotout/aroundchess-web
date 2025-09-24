1cf0b0805de394b075a814c0a850c345a1e5dac6
naufal <naufal@kemang.sg>
2025-09-24 11:18:29 +0700

    implement facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "sonner";
 import "./globals.css";
 import { AuthProvider } from "../context/AuthContext";
 import React from "react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "AroundChess",
@@ -21,8 +22,34 @@ export default function RootLayout({
         <meta name="color-scheme" content="light" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <script src="/stockfish.js" defer></script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1143400640994363&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body>
+         <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+        >
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1143400640994363');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         {/* <React.StrictMode> */}
         <AuthProvider>
           {children}
