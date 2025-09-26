8e452204464bc9c9801a9440d8dd042faa2c3366
NaufalKemang <naufal@kemang.sg>
2025-09-26 10:19:57 +0700

    change to get a special discount

--- components/CountdownTimer/CountdownTimerDiscount.tsx
+++ components/CountdownTimer/CountdownTimerDiscount.tsx
@@ -86,7 +86,7 @@ export default function CountdownTimerDiscount() {
   return (
     <div className="relative flex flex-col sm:w-[466px] sm:h-[126px] items-center space-y-3 bg-gradient-to-r from-[#66FEB7] via-[#00FE87] to-[#66FEB7] border-2 border-[#e6f7f3] border-dashed rounded-[8px] p-[8px]">
       <p className="text-[14px] sm:text-[12px] lg:text-[14px] font-medium text-center text-black block">
-        Get <span className="text-[#221AE9] font-bold">Special Discount </span>now
+        Get a <span className="text-[#221AE9] font-bold">Special Discount </span>now
         on our 12 Months Premium Subscription.
         <span className="text-[10px] sm:text-[12px] lg:text-[14px] font-medium text-center text-black block">
           Offer expires in...
