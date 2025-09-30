d8afc1086518e74bed2c95637eeb6fc788dcdbb2
naufal <naufal@kemang.sg>
2025-09-30 16:45:01 +0700

    change title web page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>AroundChess</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="color-scheme" content="light" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
