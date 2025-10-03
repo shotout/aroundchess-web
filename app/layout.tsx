5d215bc6048e303234197ba02c227e7089092929
Gerry <gerry.ahmadi@gmail.com>
2025-10-03 15:52:42 +0700

    fix GA4 implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,10 +50,20 @@ export default function RootLayout({
           </Script>
         )}
         {setting.analytics && (
-          <Script
-            id="google-analytics"
-            strategy="afterInteractive"
-            src="https://www.googletagmanager.com/gtag/js?id=G-C2DL4G5MEB" />
+          <>
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+              src="https://www.googletagmanager.com/gtag/js?id=G-C2DL4G5MEB" />
+            <Script id="google-analytics-init" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-C2DL4G5MEB');
+              `}
+            </Script>
+          </>
         )}
 
         {/* <React.StrictMode> */}
