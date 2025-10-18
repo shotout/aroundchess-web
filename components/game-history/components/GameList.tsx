e14758362326a6fe9660aed516641da6b7a4dfb8
NaufalKemang <naufal@kemang.sg>
2025-10-18 11:38:33 +0700

    perbaikan loader button

--- components/game-history/components/GameList.tsx
+++ components/game-history/components/GameList.tsx
@@ -301,7 +301,7 @@ const GamesList: React.FC<GamesListProps> = ({
       switch (job.status) {
         case "pending":
         case "processing": {
-          const pct = job.progress > 0 ? `${job.progress}%` : "In Progress";
+          const pct = job.progress > 0 ? `${job.progress}%` : "On Progress";
           return {
             text: pct,
             icon: <Loader2 className="h-4 w-4 mr-2 animate-spin" />,
@@ -533,7 +533,10 @@ const GamesList: React.FC<GamesListProps> = ({
                         className={`${btn.className} ${
                           (disabled || autoStartGameId == game.id) &&
                           "bg-gray-700"
-                        } h-8 w-full rounded-3xl ${btn.text === "Just one more moment..." && 'text-[10px]'} text-xs flex justify-center items-center transition-colors duration-150`}
+                        } h-8 w-full rounded-3xl ${
+                          btn.text === "Just one more moment..." &&
+                          "text-[10px]"
+                        } text-xs flex justify-center items-center transition-colors duration-150`}
                         onClick={btn.onClick}
                         disabled={
                           autoStartGameId == game.id ||
@@ -544,12 +547,26 @@ const GamesList: React.FC<GamesListProps> = ({
                         }
                       >
                         {(disabled && game.id == gameId) ||
-                        autoStartGameId == game.id ? (
-                          <Loader2 className="h-4 w-4 mr-1 animate-spin" />
+                        autoStartGameId == game.id ||btn.text === "Just one more moment..."? (
+                          <Loader2
+                            className={`${
+                              btn.text === "Just one more moment..." &&
+                              "block sm:hidden"
+                            } h-4 w-4 mr-1 animate-spin`}
+                          />
                         ) : (
                           btn.icon
                         )}
-                        {autoStartGameId == game.id ? "Processing":btn.text}
+                        <span className="hidden sm:block">
+                          {autoStartGameId == game.id ? "Processing" : btn.text}
+                        </span>
+                        <span className="block sm:hidden">
+                          {autoStartGameId == game.id
+                            ? "Processing"
+                            : btn.text.includes("%")
+                            ? "On Progress: " + btn.text
+                            : ""}
+                        </span>
                       </button>
                     );
                   })()}
