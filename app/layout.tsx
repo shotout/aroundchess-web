320248c7e1a496b509b4ad7d1e794393f75145f3
NaufalKemang <naufal@kemang.sg>
2025-10-21 21:24:06 +0700

    perbaikan build

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "sonner";
 import "./globals.css";
 import { AuthProvider } from "../context/AuthContext";
 import { TutorialProvider } from "../components/TutorialProvider";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import Script from "next/script";
 import { useModalSetting } from "./store/cookiesSetting";
 
@@ -14,29 +14,33 @@ export default function RootLayout({
 }) {
   const { setting } = useModalSetting();
   return (
-    <html lang="en">
-      <head>
-        <title>AroundChess</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="color-scheme" content="light" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <script src="/stockfish.js" defer></script>
-        {setting.marketing && (
-          <noscript>
-            <img
-              height="1"
-              width="1"
-              style={{ display: "none" }}
-              src="https://www.facebook.com/tr?id=1143400640994363&ev=PageView&noscript=1"
-              alt=""
-            />
-          </noscript>
-        )}
-      </head>
-      <body>
-        {setting.marketing && (
-          <Script id="facebook-pixel" strategy="afterInteractive">
-            {`
+    <Suspense>
+      <html lang="en">
+        <head>
+          <title>AroundChess</title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="color-scheme" content="light" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <script src="/stockfish.js" defer></script>
+          {setting.marketing && (
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src="https://www.facebook.com/tr?id=1143400640994363&ev=PageView&noscript=1"
+                alt=""
+              />
+            </noscript>
+          )}
+        </head>
+        <body>
+          {setting.marketing && (
+            <Script id="facebook-pixel" strategy="afterInteractive">
+              {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -48,34 +52,36 @@ export default function RootLayout({
             fbq('init', '1143400640994363');
             fbq('track', 'PageView');
           `}
-          </Script>
-        )}
-        {setting.analytics && (
-          <>
-            <Script
-              id="google-analytics"
-              strategy="afterInteractive"
-              src="https://www.googletagmanager.com/gtag/js?id=G-C2DL4G5MEB" />
-            <Script id="google-analytics-init" strategy="afterInteractive">
-              {`
+            </Script>
+          )}
+          {setting.analytics && (
+            <>
+              <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=G-C2DL4G5MEB"
+              />
+              <Script id="google-analytics-init" strategy="afterInteractive">
+                {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', 'G-C2DL4G5MEB');
               `}
-            </Script>
-          </>
-        )}
+              </Script>
+            </>
+          )}
 
-        {/* <React.StrictMode> */}
-        <AuthProvider>
-          <TutorialProvider>
-            {children}
-            <Toaster />
-          </TutorialProvider>
-        </AuthProvider>
-        {/* </React.StrictMode> */}
-      </body>
-    </html>
+          {/* <React.StrictMode> */}
+          <AuthProvider>
+            <TutorialProvider>
+              {children}
+              <Toaster />
+            </TutorialProvider>
+          </AuthProvider>
+          {/* </React.StrictMode> */}
+        </body>
+      </html>
+    </Suspense>
   );
 }
