5d56a3da09da6bd06d1ca31f0b130a69f790d44b
Toni Tri Supriadi <toni@stebasia.com>
2025-12-15 23:53:22 +0700

    fix issue: 15-16

--- app/playground/endgame-training/components/TabSelector.tsx
+++ app/playground/endgame-training/components/TabSelector.tsx
@@ -13,11 +13,11 @@ const TabSelector: React.FC<TabSelectorProps> = ({
   onTabChange,
 }) => {
   return (
-    <Card className="flex gap-3 bg-[#F9FAFC] border border-gray-100 p-1 md:p-2 w-full md:max-w-96 ">
+    <div className="flex md:gap-3 bg-[#E6F7FE] md:bg-[#F9FAFC] md:border md:border-gray-100 p-0 md:p-[6px] w-[calc(100%+24px)] md:w-1/2 rounded-0 md:rounded-lg mx-[-24px] md:mx-0">
       <button
-        className={`flex-1 py-1.5 flex items-center justify-center gap-1 rounded-sm text-[14px] --xs ${
+        className={`flex-1 py-[8px] md:py-[6px] px-[6px] md:px-[10px] flex items-center justify-center gap-1 md:rounded-sm text-[13px] md:text-[14px] --xs ${
           activeTab === "board"
-            ? "bg-white border shadow-sm font-semibold border-gray-300 text-blue-base"
+            ? "md:bg-white md:border md:shadow-sm font-semibold md:border-gray-300 text-blue-base border-b-[2px] border-blue-base"
             : ""
         }`}
         onClick={() => onTabChange("board")}
@@ -31,13 +31,14 @@ const TabSelector: React.FC<TabSelectorProps> = ({
           alt="Board icon"
           width={30}
           height={30}
+          className="w-[16px] md:w-[24px] h-[16px] md:h-[24px]"
         />
         <h1>Board Presentation</h1>
       </button>
       <button
-        className={`flex-1 py-1.5 flex items-center justify-center gap-1 rounded-sm text-[14px] --xs ${
+        className={`flex-1 py-[8px] md:py-[6px] px-[6px] md:px-[10px] flex items-center justify-center gap-1 md:rounded-sm text-[13px] md:text-[14px] --xs ${
           activeTab === "move"
-            ? "bg-white border shadow-sm font-semibold border-gray-300 text-blue-base"
+            ? "md:bg-white md:border md:shadow-sm font-semibold md:border-gray-300 text-blue-base border-b-[2px] border-blue-base"
             : ""
         }`}
         onClick={() => onTabChange("move")}
@@ -51,10 +52,11 @@ const TabSelector: React.FC<TabSelectorProps> = ({
           alt="Moves icon"
           width={30}
           height={30}
+          className="w-[16px] md:w-[24px] h-[16px] md:h-[24px]"
         />
         <h1>Moves Until Checkmate</h1>
       </button>
-    </Card>
+    </div>
   );
 };
 
--- app/playground/endgame-training/components/board/CategoryItem.tsx
+++ app/playground/endgame-training/components/board/CategoryItem.tsx
@@ -14,9 +14,9 @@ export const CategoryItem: React.FC<CategoryItemProps> = ({
   const slug = category.name.toLowerCase().replace(/\s+/g, "-");
 
   return (
-    <div className="rounded-md overflow-hidden p-4 xl:p-5 border border-gray-200 bg-[#F9FAFC] flex max-w-auto hover:bg-blue-base/10 transition-all md:max-h-[226px] md:h-52">
-      <div className="w-full xl:border xl:p-5 rounded-md xl:bg-white flex flex-col md:flex-row md:gap-x-5">
-        <div className="w-full md:w-1/3 flex items-center xl:bg-white rounded-md xl:justify-center mb-4 md:mb-0">
+    <div className={`relative rounded-md overflow-hidden p-[16px] pb-[48px] lg:pb-5 lg:p-5 border border-gray-200 bg-[#F9FAFC] flex max-w-auto lg:hover:bg-blue-base/10 transition-all md:max-h-[226px] md:h-52 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.12)] md:shadow-none`}>
+      <div onClick={() => onCategorySelect(slug)} className="relative z-10 w-full xl:border lg:p-[16px] rounded-md xl:bg-white flex flex-col md:flex-row md:gap-x-[16px] md:shadow-[0px_2px_4px_0px_rgba(0,0,0,0.12)] cursor-pointer">
+        <div className="w-full md:w-1/3 hidden md:flex items-center xl:bg-white rounded-md xl:justify-center mb-4 md:mb-0">
           <Image
             src={`/endgame-training/${category.icons}`}
             alt={`${category.name} icon`}
@@ -24,6 +24,7 @@ export const CategoryItem: React.FC<CategoryItemProps> = ({
             height={120}
             className="hidden xl:flex"
           />
+
           <Image
             src={`/endgame-training/${category.icons}`}
             alt={`${category.name} icon`}
@@ -32,23 +33,24 @@ export const CategoryItem: React.FC<CategoryItemProps> = ({
             className="xl:hidden flex"
           />
         </div>
-        <div className="w-full md:w-1/2 flex flex-col justify-between space-y-2 md:space-y-0">
+
+        <div className="w-full md:w-2/3 flex flex-col justify-between space-y-2 md:space-y-0">
           <div className="flex flex-col">
-            <h3 className="font-semibold xl:font-bold text-base xl:text-2xl text-black">
+            <h3 className="font-normal md:font-bold text-[16px] md:mb-[8px] lg:text-[20px] text-black">
               {category.name.replace("-"," & ")}
             </h3>
           </div>
           <div>
-            <p className="text-[14px] --xs text-gray-600">
+            <p className="text-[14px] leading-[120%] mb-[8px] --xs text-gray-600">
               {category.name === "Basic"
                 ? "Simple Endgame Scenarios with only the King as Enemy"
                 : `Multiple Endgame Scenarios involving the ${category.name}.`}
             </p>
           </div>
-          <div>
+          <div className="hidden md:flex">
             <button
               onClick={() => onCategorySelect(slug)}
-              className="btn-primary w-full xl:max-w-[100px] text-white py-1 xl:py-2 px-3 rounded-full text-[14px] --sm font-medium flex items-center justify-center"
+              className="btn-primary w-full text-white py-1 lg:py-[6px] px-3 rounded-full text-[14px] --sm font-medium flex items-center justify-center"
             >
               <span className="mr-1">Select</span>
               <ArrowRight className="w-3 h-3" />
@@ -56,6 +58,7 @@ export const CategoryItem: React.FC<CategoryItemProps> = ({
           </div>
         </div>
       </div>
+      <Image src={`/endgame-training/mobile/${category.name}.jpg`} alt="..." width={100} height={100} className="md:hidden absolute w-full h-full bottom-0 left-0 z-0" />
     </div>
   );
 };
