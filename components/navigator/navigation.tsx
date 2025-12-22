dbe7ad91032a60663b3715486adf4a575c2ccad6
Toni Tri Supriadi <toni@stebasia.com>
2025-12-22 07:48:17 +0700

    fix issue

--- components/navigator/navigation.tsx
+++ components/navigator/navigation.tsx
@@ -227,7 +227,7 @@ export default function Navigation({
               />
 
               <motion.div
-                className="fixed inset-y-0 right-0 z-50 w-[82%] bg-white border-l border-gray-200 shadow-xl overflow-hidden"
+                className="fixed inset-y-0 right-0 z-50 w-[82%] bg-white border-l border-gray-200 shadow-xl"
                 variants={sidebarVariants}
                 initial="hidden"
                 animate="visible"
