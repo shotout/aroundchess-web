e96a9d9264857a349aabdbc822a5e80d4ad9cd22
Toni Tri Supriadi <toni@stebasia.com>
2025-12-22 07:39:07 +0700

    fix issue

--- components/analysis/onboarding/PremiumSubscription.tsx
+++ components/analysis/onboarding/PremiumSubscription.tsx
@@ -451,14 +451,14 @@ export const PremiumSubsContent: React.FC<{
       
       <div
         ref={containerRef}
-        className={`flex w-[84vw] mx-auto overflow-x-scroll gap-4 pt-[8px] lg:overflow-x-hidden sm:grid sm:gap-4 sm:grid-cols-2 snap-x snap-mandatory scroll-smooth`}
+        className={`flex w-[84vw] md:w-auto md:min-w-[320px] mx-auto overflow-x-scroll gap-4 pt-[8px] lg:overflow-x-hidden sm:grid sm:gap-4 sm:grid-cols-2 snap-x snap-mandatory scroll-smooth`}
         style={{ WebkitOverflowScrolling: 'touch' }}
       >
         {/* free */}
         {(isMobile || packageFilter === "monthly") && (
           <div
             ref={freeCardRef}
-            className="w-[84vw] md:min-w-[320px] lg:min-w-full bg-white p-4 rounded-xl border border-gray-200 shadow-sm flex flex-col md:order-none snap-center flex-shrink-0"
+            className="w-[84vw] md:w-auto md:min-w-[320px] lg:min-w-full bg-white p-4 rounded-xl border border-gray-200 shadow-sm flex flex-col md:order-none snap-center flex-shrink-0"
           >
             <div className="flex items-center gap-3 mb-3">
               <div className="p-2 bg-blue-50 rounded-full">
@@ -531,7 +531,7 @@ export const PremiumSubsContent: React.FC<{
         {(isMobile ||
           packageFilter === "monthly" ||
           packageFilter === "yearly") && (
-          <div ref={monthlyCardRef} className="w-[84vw] md:min-w-[320px] lg:min-w-full bg-gradient-to-br from-[#130F83] to-[#00FFBB] text-white p-4 md:order-none rounded-xl shadow-md relative flex flex-col snap-center flex-shrink-0">
+          <div ref={monthlyCardRef} className="w-[84vw] md:w-auto md:min-w-[320px] lg:min-w-full bg-gradient-to-br from-[#130F83] to-[#00FFBB] text-white p-4 md:order-none rounded-xl shadow-md relative flex flex-col snap-center flex-shrink-0">
             {/* <div className="absolute -top-2 left-0 right-0 flex justify-center">
               <div className="bg-[#A855F7] px-3 py-1 rounded-full text-[14px] --xs font-medium">
                 For frequent Chess Players
@@ -669,7 +669,7 @@ export const PremiumSubsContent: React.FC<{
         {/* yearly */}
         {(isMobile || packageFilter === "yearly") && (
           <div ref={yearlyCardRef}
-            className="w-[84vw] md:min-w-[320px] lg:min-w-full bg-gradient-to-br from-[#221AE9] to-[#25CEDA] text-white p-4 md:order-none rounded-xl shadow-md relative flex flex-col snap-center flex-shrink-0">
+            className="w-[84vw] md:w-auto md:min-w-[320px] lg:min-w-full bg-gradient-to-br from-[#221AE9] to-[#25CEDA] text-white p-4 md:order-none rounded-xl shadow-md relative flex flex-col snap-center flex-shrink-0">
             <div className="absolute -top-2 left-0 right-0 flex justify-center">
               <div className="bg-[#A855F7] px-3 py-1 rounded-full text-[14px] --xs font-medium">
                 For frequent Chess Players
