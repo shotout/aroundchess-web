0fb208bc8fa6d48e89301b5baee3cf2a07bb18c8
Giuseppe Paternicola <github@aroundchess.com>
2026-01-21 15:14:55 +0100

    Remove Brevo

--- app/my-game-history/page.tsx
+++ app/my-game-history/page.tsx
@@ -6,7 +6,6 @@ import Navigation from "@/components/navigator/navigation";
 import { usePgnStore } from "../store/zustandStore";
 import { useEffect } from "react";
 import { trackCustomEvent } from "../utils/facebookPixel";
-import Script from "next/script";
 
 export default function Page() {
   const { isLoading } = usePgnStore();
@@ -15,23 +14,6 @@ useEffect(() => {
   }, []);
   return (
     <>
-      <Script 
-        src="https://cdn.brevo.com/js/sdk-loader.js" 
-        strategy="afterInteractive"
-        async
-      />
-      <Script id="brevo-tracker" strategy="afterInteractive">
-        {`
-          // Version: 2.0
-          window.Brevo = window.Brevo || [];
-          Brevo.push([
-            "init",
-            {
-              client_key: "j2yltq6rlqd3ojhemkgb7x1z"
-            }
-          ]);
-        `}
-      </Script>
       {isLoading == true ? (
         <LoadingPage />
       ) : (
