eb6c4442e6742fea5deff8e18d63db4d49f1626e
Giuseppe Paternicola <github@aroundchess.com>
2026-03-06 15:53:12 +0100

    fix sidebar cut off

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -7,8 +7,10 @@ import { cn } from "@/lib/utils";
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> & {
+    showScrollbar?: boolean;
+  }
+>(({ className, children, showScrollbar = true, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
     className={cn("relative overflow-hidden", className)}
@@ -17,8 +19,8 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
-    <ScrollAreaPrimitive.Corner />
+    {showScrollbar && <ScrollBar />}
+    {showScrollbar && <ScrollAreaPrimitive.Corner />}
   </ScrollAreaPrimitive.Root>
 ));
 ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
