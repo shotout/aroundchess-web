a4cd1008f5e765f0ff2e85bcc9bc38eed3b78f68
Giuseppe Paternicola <github@aroundchess.com>
2026-03-07 17:57:43 +0100

    adjust the Get Offer redirection

--- constants/marchOffer.ts
+++ constants/marchOffer.ts
@@ -2,6 +2,7 @@ export const MARCH_OFFER_CAMPAIGN_ID = "march_2026";
 export const MARCH_OFFER_DIALOG_SESSION_KEY = "showMarchOfferModal";
 export const MARCH_OFFER_DIALOG_DELAY_MS = 1500;
 export const MARCH_OFFER_DIALOG_MAX_WAIT_MS = 6000;
+export const MARCH_OFFER_DISCOUNT_PERCENT = 55;
 export const MARCH_OFFER_END_DATE_LABEL = "31.03.2026";
 export const MARCH_OFFER_END_MS = new Date("2026-04-01T00:00:00").getTime();
 
